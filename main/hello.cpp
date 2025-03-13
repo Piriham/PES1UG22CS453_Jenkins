85af065b83d6bba64d6d01483ff8aa13e4787b73
Piriham <77205713+Piriham@users.noreply.github.com>
2025-03-13 20:09:51 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-using namespace std;
 
-const string APP_NAME = "Jenkins Test";
+using namespace std;
 
 int main() {
     cout << "Hello, World!\n";
     cout << "Hello, Jenkins!\n";
-    cout << "The program has been built and executed successfully in " << APP_NAME << ".\n";
+    cout << "The program has been built and executed successfully.\n";
     return 0;
 }
